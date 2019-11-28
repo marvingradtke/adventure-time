14c97e4fd16353234c4aed121d9be985691ce9ac
marvingradtke <m.gradtke@gmail.com>
2019-11-28 15:30:07 +0100

    Navigation styled and favicon added

--- src/App.js
+++ src/App.js
@@ -2,25 +2,54 @@ import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import { ThemeProvider } from "emotion-theming";
 import defaultTheme from "./themes/defaultTheme";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import BottomNavigation from "./components/BottomNavigation";
+import Title from "./components/Title";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  flex-grow: 1;
+`;
+
+const Header = styled.header`
+  height: 80px;
+  background: ${props => props.theme.colors.secondary};
+`;
+
+const Footer = styled.footer`
+  height: 80px;
+  background: ${props => props.theme.colors.secondary};
+`;
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
       <Router>
-        <Switch>
-          <Route path="/new">
-            <div>New</div>
-          </Route>
-          <Route path="/">
-            <div>Adventure Time</div>
-          </Route>
-        </Switch>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/new">New</Link>
-        </div>
+        <Container>
+          <Header>
+            <Title>Adventure Time</Title>
+          </Header>
+          <Main>
+            <Switch>
+              <Route path="/Character">
+                <div>Character</div>
+              </Route>
+              <Route path="/">
+                <div>Time</div>
+              </Route>
+            </Switch>
+          </Main>
+          <Footer>
+            <BottomNavigation />
+          </Footer>
+        </Container>
       </Router>
     </ThemeProvider>
   );
--- src/themes/defaultTheme.js
+++ src/themes/defaultTheme.js
@@ -1,7 +1,9 @@
 const defaultTheme = {
   colors: {
     background: "#109c89",
-    text: "#bae8e2"
+    text: "#bae8e2",
+    secondary: "#43635f",
+    primary: "#6cd9cc"
   }
 };
 
